4aad7c4ade8e5899a79b7bad04fafc0077aca3aa
joserosalessueiro <jrsueiro@yahoo.es>
2024-02-28 20:26:48 +0100

    trabajando en cookie

--- www/UD05_Eventos_Formularios/scriptSolucion.js
+++ www/UD05_Eventos_Formularios/scriptSolucion.js
@@ -1,14 +1,42 @@
-function getCookie() {
+function getCookie(nombre) {
+    var name = nombre + "=";
+    //decodifica el valor de document.cookie, que contiene todas las cookies del documento
+    var decodedCookie = decodeURIComponent(document.cookie);
+    //separamos el contenido por ";"
+    var partesCookie = decodedCookie.split(';');
 
+    for (var i = 0; i < partesCookie.length; i++) {
+        var parte = partesCookie[i];
+        while (parte.charAt(0) == ' ') {
+            parte = parte.substring(1);
+        }
+        if (parte.indexOf(name) == 0) {
+            return parte.substring(name.length, parte.length);
+        }
+    }
+    return "";
 }
 
-function setCookie(name, value, time) {
+
+function setCookie() {
     var name = 'contador';
     var value = 1;
-    var path = '.';
-    var d = new Date();
+    var expire = new Date();
+    expire.setMonth(expire.getMonth() + 1); // Agrega un mes a la fecha actual
+
+    document.cookie = name + "=" + value + "; expires=" + expire.toUTCString();
+}
+
+function contadorCookie() {
+    var contador = parseInt(getCookie('contador'));
+
+    if (contador || contador === 0) {
+        contador++;
+    } else {
+        setCookie()
+    }
     
-    d.setTime(d.getTime() + (time * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    document.getElementById('intentos').innerText = "Intentos de envío del formulario: " + contador;
+    alert(contador);
+
 }
