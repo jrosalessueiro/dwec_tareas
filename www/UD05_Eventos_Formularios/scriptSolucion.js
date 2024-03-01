84d5758c9d17a09d768687429aa63d28c7d1924d
joserosalessueiro <jrsueiro@yahoo.es>
2024-03-01 20:27:56 +0100

    validaciones hechas pero no funciona

--- www/UD05_Eventos_Formularios/scriptSolucion.js
+++ www/UD05_Eventos_Formularios/scriptSolucion.js
@@ -1,3 +1,4 @@
+
 function funcionesSolucion() {
     contadorCookie();
     validateForm();
@@ -8,12 +9,41 @@ function funcionesSolucion() {
 function validateForm() {
     // Objecto que contiene pares id => error, ej. {nombre => "Nombre no valido", dni => "DNI no valido"}
     var errors = {};
-    if (!validateText(getElementById("nombre").value)) {
+
+    if (!validateText(document.getElementById("nombre").value)) {
         errors.nombre = "Nombre no valido, debe ser un texto.";
     }
 
-    if (!validateText(getElementById("apellidos").value)) {
+    if (!validateText(document.getElementById("apellidos").value)) {
         errors.nombre = "Apellidos no validos, deben ser un texto.";
+    }
+
+    if (!validateAge(document.getElementById("edad").value)) {
+        errors.nombre = "Edad no válida, debe ser un número entre 0 y 105 años.";
+    }
+
+    if (!validateDni(document.getElementById("nif").value)) {
+        errors.nombre = "DNI no válida, deben ser 8 dígitos, un guión y una letra.";
+    }
+
+    if (!validateEmail(document.getElementById("email").value)) {
+        errors.nombre = "Email no válido, compruebe el valor introducido.";
+    }
+
+    if (!validateState(document.getElementById("provincia").value)) {
+        errors.nombre = "La provincia no es válida, debe marcar una de las opciones.";
+    }
+
+    if (!validateDate(document.getElementById("fecha").value)) {
+        errors.nombre = "La fecha no es válida, debe tener el formato dd/mm/aaaa o dd-mm-aaaa.";
+    }
+
+    if (!validatePhone(document.getElementById("telefono").value)) {
+        errors.nombre = "El teléfono no es válido, debe contener 9 dígitos.";
+    }
+
+    if (!validateHour(document.getElementById("hora").value)) {
+        errors.nombre = "La hora no es válida, debe ser introducida en el formato hh:mm.";
     }
 
     if (errors != {}) {
@@ -35,8 +65,101 @@ function validateText(text) {
     }
 }
 
+function validateAge(age) {
 
+    if (!isNaN(age) && age >= 0 && age <= 105) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function validateDni(dni) {
+    // ^ comienzo de la cadena.
+    // \d{8} ocho dígitos.
+    // - un guión.
+    // [a-zA-Z] una letra, mayúscula o minúscula.
+    // $ fin de la cadena.
+    var dniValido = /^(\d{8})-[a-zA-Z]$/;
+
+    if (dniValido.test(dni)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    // ^ comienzo de la cadena.
+    // [a-z]{1,15} entre 1 y 15 letras minúsculas.
+    // @ arroba.
+    // [a-z]{1,10} entre 1 y 10 letras minúsculas.
+    // . un punto
+    //(com|es|gal|net|org|info) alguna delos dominios más comunes 
+    //$ fin de la cadena
+    var dniValido = /^[a-z]{1,15}@[a-z]{1,10}.(com|es|gal|net|org|info)$/;
+
+    if (dniValido.test(dni)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateState(state) {
+
+    if (state != "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateDate(date) {
+    // ^ comienzo de la cadena.
+    // d{2} dos dígitos.
+    // / barra o - guión
+    // d{2} dos dígitos.
+    // / barra o - guión
+    // d{4} cuatro dígitos.
+    //$ fin de la cadena
+    var dateValida1 = /^\d{2}\/\d{2}\/\d{4}$/;
+    var dateValida2 = /^\d{2}\-\d{2}\-\d{4}$/;
+
+    if (dateValida1.test(date) || dateValida2.test(date)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validatePhone(phone) {
+    // ^ comienzo de la cadena.
+    // d{9} nueve dígitos.
+    //$ fin de la cadena
+    var phoneValido = /^(\d{9})$/;
+
+    if (phoneValido.test(phone)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateHour(hour) {
+    // ^ comienzo de la cadena.
+    //d{2} dos dígitos.
+    // : dos puntos
+    // d{2} dos dígitos.
+    //$ fin de la cadena
+    var hourValida = /^\d{2}\:\d{2}\$/;
+
+    if (hourValida.test(hour)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 window.onload = function () {
     document.getElementById("nombre").addEventListener("blur", function () {
@@ -48,7 +171,18 @@ window.onload = function () {
         var apellidos = document.getElementById("apellidos");
         apellidos.value = apellidos.value.toUpperCase();
     });
+
+    document.getElementById("formulario").addEventListener("submit", function (event) {
+        var confirmation = confirm("¿Está seguro de que desea enviar el formulario?");
+
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
 }
+
+
+//Cookie contador de intentos de envío del formulario
 function getCookie(nombre) {
     var name = nombre + "=";
     //decodifica el valor de document.cookie, que contiene todas las cookies del documento
@@ -67,9 +201,6 @@ function getCookie(nombre) {
     }
     return "";
 }
-
-
-//Cookie contador de intentos de envío del formulario
 
 function setCookie(value) {
     var name = 'contador';
